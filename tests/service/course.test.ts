6150e31c684da753bc6944dde9b46ce7417ef892
Stanislav Minko <minkostas@gmail.com>
2023-07-21 23:04:27 +0300

    educationalPlatform: unit_tests:getAll, getById, Update, Delete

--- tests/service/course.test.ts
+++ tests/service/course.test.ts
@@ -1,10 +1,61 @@
-import { createCourse } from "../../src/service/course.service"
+import { createCourse, deleteCourseById, getCourse, getCourseById, updateCourse } from "../../src/service/course.service"
 import * as repository from '../../src/repository/course.repository'
-describe('test_getCourse', () => {
-    test('Succes', async () => {
+import exp from "constants";
+
+describe('createCourse function', () => {
+    test('Success', async () => {
         const funcRepo = jest.spyOn(repository, 'createCourseDB');
-        funcRepo.mockResolvedValue([{ id: 1, course: " test_course" }]);
-        const result = await createCourse('course');
+        funcRepo.mockResolvedValue([{ id: 1, course: 'test_course' }]);
+        const result = await createCourse('test_course');
+
         expect(funcRepo).toHaveBeenCalled();
+        expect(result).toEqual([{ id: 1, course: "test_course" }]);
+        expect(result).toHaveLength(1);
+    })
+})
+
+describe('getCourse', () => {
+    test("Success", async () => {
+        const funcRepo = jest.spyOn(repository, "getCourseDB");
+        funcRepo.mockResolvedValue([{ id: 1, course: "course1" }, { id: 2, course: "course2" }, { id: 3, course: "course3" }]);
+
+        const result = await getCourse();
+        expect(funcRepo).toHaveBeenCalled(); //проверка, что что функция вызывалась
+        expect(result).toEqual([{ id: 1, course: "course1" }, { id: 2, course: "course2" }, { id: 3, course: "course3" }]);
+        expect(result.length).toBeGreaterThan(0);
+    })
+})
+
+describe("getCourseById function", () => {
+    test('Success', async () => {
+        const funcRepo = jest.spyOn(repository, "getCourseByIdDB");
+        funcRepo.mockResolvedValue([{ id: 1, course: "test_course" }])//предполагаемый возвращаемый результат
+        const result = await getCourseById(1);
+        expect(funcRepo).toHaveBeenCalled();
+        expect(result).toEqual([{ id: 1, course: "test_course" }]);
+        expect(result.length).toBeGreaterThan(0);
+    })
+})
+
+describe("updateCourse function", ()=>{
+    test("Success", async ()=>{
+        const funcRepo = jest.spyOn(repository, "updateCourseDB");
+        funcRepo.mockResolvedValue([{id:1, course:"course_updated"}]);
+        const result = await updateCourse("course", 1 );
+        expect(funcRepo).toHaveBeenCalled();
+        expect(result).toEqual([{id:1, course:"course_updated"}]);
+        expect(result.length).toBeGreaterThan(0);
+        expect(result[0].course).toBe("course_updated");
+    })
+})
+
+describe("deleteCourseById function", ()=>{
+    test("Success", async ()=>{
+        const funcRepo = jest.spyOn(repository,"deleteCourseByIdDB");
+        funcRepo.mockResolvedValue([{id:1, course:"course_1_to_delete"}]);
+        const result = await deleteCourseById(1);
+        expect(funcRepo).toHaveBeenCalled();
+        expect(result).toEqual([{id:1, course:"course_1_to_delete"}]);
+        expect(result.length).toBeGreaterThan(0);
     })
 })
