46c10a730558c0a80ee75981f912dafeee2afd5e
Stanislav Minko <minkostas@gmail.com>
2023-08-04 11:33:07 +0300

    educationalPlatform: components_Preview.js

---
+++ client/src/components/Preview/Preview.js
@@ -0,0 +1,25 @@
+import style from "./style.module.css";
+
+function Preview() {
+  return (
+    <div className={style.wrapper}>
+      <div className={style.info}>
+        <p>E-COURSE PLATFORM</p>
+        <h1>Learning and teaching online,made easy.</h1>
+        <p>Any subject, in any language, on any device, for all ages!</p>
+        <div className={style.btnAbout}>About platform</div>
+        <div className={style.statistic}>
+            <div>
+                <div className={style.thunder}></div>
+                <div>600<span>+</span></div>
+            </div>
+            <p>students</p>
+        </div>
+      </div>
+      <div className={style.image}></div>
+
+    </div>
+  );
+}
+
+export default Preview;
