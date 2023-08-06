8b4cb64543825f0fbc890007bdf960c5428cb081
Stanislav Minko <minkostas@gmail.com>
2023-08-06 12:57:24 +0300

    educationalPlatform: components_Preview.css_2

--- client/src/components/Preview/Preview.js
+++ client/src/components/Preview/Preview.js
@@ -2,22 +2,41 @@ import style from "./style.module.css";
 
 function Preview() {
   return (
-    <div className={style.wrapper}>
-      <div className={style.info}>
-        <p className={style.ecourse}>E-COURSE PLATFORM</p>
-        <h1>Learning and teaching online, made easy.</h1>
-        <p className={style.text}>Any subject, in any language, on any device, for all ages!</p>
-        <div className={style.btnAbout}>About platform</div>
-        <div className={style.statistic}>
+    <div>
+      <div className={style.preview}>
+        <div className={style.info}>
+          <p className={style.ecourse}>E-COURSE PLATFORM</p>
+          <h1>Learning and teaching online, made easy.</h1>
+          <p className={style.text}>
+            Any subject, in any language, on any device, for all ages!
+          </p>
+          <div className={style.btnAbout}>About platform</div>
+          <div className={style.statistic}>
             <div>
-                <div className={style.thunder}></div>
-                <div>600<span>+</span></div>
+              <div className={style.thunder}></div>
+              <div>
+                600<span>+</span>
+              </div>
             </div>
             <p>students</p>
+          </div>
+        </div>
+        <div className={style.image}></div>
+      </div>
+
+      <div className={style.wrapperBCG}>
+      <div className={style.language}>
+        <div className={style.imgLanguages}></div>
+        <div className={style.information}>
+          <h1>Learn a language in a playful way</h1>
+          <p>Make learning programming languages more fun with mini-games</p>
+          <div className={style.btnImgs}>
+            <div className={style.imgSprints}></div>
+            <div className={style.imgTasks}></div>
+          </div>
         </div>
       </div>
-      <div className={style.image}></div>
-
+      </div>
     </div>
   );
 }
