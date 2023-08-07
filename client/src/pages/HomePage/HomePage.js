8944a5500d3e33a8ea3fa35231919c2f116f9fdb
Stanislav Minko <minkostas@gmail.com>
2023-08-07 10:23:09 +0300

    educationalPlatform: routing

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,6 +1,6 @@
 import Header from "../../components/Header/Header"
 import Preview from "../../components/Preview/Preview";
-import Footer from '../../components/Footer/Footer';
+import Footer from "../../components/Footer/Footer";
 function HomePage (){
     return (
         <div>
