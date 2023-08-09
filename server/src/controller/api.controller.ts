3fd37fc65238edac71f308129ecab1fb4536f6ca
Stanislav Minko <minkostas@gmail.com>
2023-08-09 20:49:49 +0300

    integration_tests: api.controller.test.ts(get/:id)

--- server/src/controller/api.controller.ts
+++ server/src/controller/api.controller.ts
@@ -2,6 +2,7 @@ import express, {Request, Response} from 'express';
 import buildResponse from '../helper/buildResponse';
 import {registration, authorizationUser} from '../service/api.service';
 import {deleteUserById} from '../service/user.service'
+import {getUserById} from '../service/user.service'
 const api = express.Router();
 
 api.post('/registration', async (req:Request, res:Response):Promise<void>=>{
@@ -26,6 +27,15 @@ api.post('/auth', async (req:Request, res:Response):Promise<void>=>{
     try {
         const{name, surname, email, pwd} = req.body;
         const data = await authorizationUser(email, pwd);
+        buildResponse(res, 200, data);
+    } catch (error:any) {
+        buildResponse(res, 400, error.message);
+    }
+})
+api.get('/registration/:id', async (req:Request, res:Response):Promise<void>=>{
+    try {
+        const{id} = req.params;
+        const data = await getUserById(Number(id));
         buildResponse(res, 200, data);
     } catch (error:any) {
         buildResponse(res, 400, error.message);
--- server/tests/controller/api.controller.test.ts
+++ server/tests/controller/api.controller.test.ts
@@ -1,7 +1,7 @@
 import request from 'supertest'
 import app from "../../src/app"
-let id:any;
-let hash_pwd:string;
+let id: any;
+let hash_pwd: string;
 
 test('POST', async () => {
     const res = await request(app).post(`/api/registration`).send({ name: "Sam1", surname: "ff", email: "sam_ff@gmail.com", pwd: "12345" });
@@ -13,17 +13,16 @@ test('POST', async () => {
     expect(res.body.length).toBe(1);
 })
 
-/* test("GET/:id", async ()=>{
+test("GET/:id", async () => {
     const res = await request(app).get(`/api/registration/${id}`);
     expect(res.status).toBe(200);
     expect(res.body).toEqual([
-        { name: "SamUser", surname: "F", email: "sam_f1@gmail.com", pwd: "12345" }
+        { id: id, name: "Sam1", surname: "ff", email: "sam_ff@gmail.com", pwd: hash_pwd }
     ])
-}) */
+})
 test('DELETE', async () => {
     const res = await request(app).delete(`/api/registration/${id}`);
     expect(res.status).toBe(200);
     console.log(res.body);
-
     expect(res.body).toEqual([{ id: id, name: "Sam1", surname: "ff", email: "sam_ff@gmail.com", pwd: hash_pwd }])
 })
