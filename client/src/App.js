4bc9b4c5b0a4019524ec19066292b670d848c67b
Stanislav Minko <minkostas@gmail.com>
2023-08-10 14:35:39 +0300

    educationalPlatform: feat: components_Auth.js refactor

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import HomePage from './pages/HomePage/HomePage';
 import {Routes, Route} from 'react-router-dom'
 import RegPage from './pages/RegPage/RegPage'
+import AuthPage from './pages/AuthPage/AuthPage';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage/>}></Route>
-      <Route path="/reg" element={<RegPage/>}> </Route>
+      <Route path={"/"} element={<HomePage/>}></Route>
+      <Route path={"/auth"} element={<AuthPage/>}> </Route>
+      <Route path={"/reg"} element={<RegPage/>}> </Route>
     </Routes>
   );
 }
