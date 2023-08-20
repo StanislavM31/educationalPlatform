8454844e55777ca71bb4a03f46b23242c7f31ca0
Stanislav Minko <minkostas@gmail.com>
2023-08-20 12:17:58 +0300

    :feat: registration-hooks_props_auth

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -1,9 +1,9 @@
 
 
 export default function Input(obj){
-    
-    const result = obj.arr.map((el)=>
-        <div>
+
+    const result = obj.arr.map((el, index)=>
+        <div key = {index}>
             <input type="text" placeholder={el}/>
         </div>
     )
--- client/src/pages/AuthPage/AuthPage.js
+++ client/src/pages/AuthPage/AuthPage.js
@@ -1,14 +1,11 @@
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import style from "./style.module.css";
+import Input from "../../components/Input/Input"
 
 function AuthPage() {
   const arr = ["email", "password"];
-  const result = arr.map((el) => (
-    <div>
-      <input type="text" placeholder={el} />
-    </div>
-  ));
+
   return (
     <div>
       <Header />
@@ -16,7 +13,7 @@ function AuthPage() {
       <div className={style.login}>
         <div className={style.loginInfo}>
           <h1>Login</h1>
-          {result}
+          <Input arr={arr}/>
           <div className={style.loginUpBtn}>Login</div>
         </div>
 
