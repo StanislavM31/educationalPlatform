6a82a3e8ded0b961eabbbb83c41e6182a88ff4e7
Stanislav Minko <minkostas@gmail.com>
2023-10-13 21:00:00 +0300

    :refactor getCourseDescription route

--- client/src/pages/CoursePage/CoursePage.js
+++ client/src/pages/CoursePage/CoursePage.js
@@ -1,17 +1,26 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import array from "../../storage/course.json";
+/* import array from "../../storage/course.json"; */
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import style from "./style.module.css";
+import axios from "axios";
 
 export default function CoursePage() {
   const { id } = useParams();
   const [element, setElement] = useState({});
   useEffect(() => {
-    const curElement = array.filter((el) => el.id === id);
-    setElement(curElement[0]);
+/*     const curElement = array.filter((el) => el.id === id);
+    setElement(curElement[0]); */
+    getCourseLesson()
   }, [id]);
+
+  async function getCourseLesson(){
+    const data = await axios.get(`http://localhost:3001/course/${id}`);
+    console.log(data.data[0]);
+    setElement(data.data[0])
+  }
+
   return (
     <>
       <Header></Header>
@@ -20,8 +29,8 @@ export default function CoursePage() {
           <div className={style.flex}>
             <div className={style.img}></div>
             <div className={style.info}>
-              <h2>{element.name}</h2>
-              <p>{element.text}</p>
+              <h2>{element.course}</h2>
+              <p>{element.description}</p>
             </div>
           </div>
 
@@ -29,7 +38,7 @@ export default function CoursePage() {
         </div>
 
         <div className={style.lesson}>
-            <h2>LESSON</h2>
+            <h2>LESSONS</h2>
           <ul>
             <li>Test</li>
             <li>Test</li>
--- client/src/pages/StudentPage/StudentPage.js
+++ client/src/pages/StudentPage/StudentPage.js
@@ -2,10 +2,10 @@ import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import style from "./style.module.css";
 import { Pagination } from "@mui/material";
-import array from "../../storage/course.json";
+/* import array from "../../storage/course.json"; */
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-
+import axios from "axios";
 
 function StudentPage() {
   const [elements, setElements] = useState([]);
@@ -13,7 +13,9 @@ function StudentPage() {
 
   const elementsPerPage = 3;
 
-  useEffect(() => setElements(array), [currentPage]);
+  useEffect(() => {
+    sendData()
+  }, [currentPage]);
 
   //Логика расчета элементов для отображения
   const indexLastElement = currentPage * elementsPerPage; //1*3, 2*3, 3*3
@@ -25,6 +27,12 @@ function StudentPage() {
     setCurrentPage(page);
   }
 
+  async function sendData(){
+    const data = await axios.get(`http://localhost:3001/course`);
+    console.log(data.data);
+    setElements(data.data)
+    return data.data;
+  }
   return (
     <>
       <Header />
@@ -40,8 +48,8 @@ function StudentPage() {
                 <div  className={style.courseCart}>
                   <div className={style.courseImg}></div>
                   <div className={style.courseOuter}>
-                    <h2>{el.name}</h2>
-                    <p>{el.text}</p>
+                    <h2>{el.course}</h2>
+                    <p>{el.description}</p>
                   </div>
                 </div>
               </Link>
@@ -54,7 +62,7 @@ function StudentPage() {
         <Pagination
           value={currentPage}
           onChange={updateData}
-          count={Math.ceil(array.length / elementsPerPage)}
+          count={Math.ceil(elements.length / elementsPerPage)}
           color="primary"
           hideNextButton={false}
         />
